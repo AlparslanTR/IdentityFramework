5b29ab8ef7a5f7f81481e47cfb8c2dbd0af52287
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-02-20 16:39:27 +0300

    Kayıt olma sayfası ve metotları tamamlandı.

---
+++ IdentityFrameworkWepApp/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using IdentityFrameworkWepApp.Data;
+using IdentityFrameworkWepApp.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityFrameworkWepApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public LoginController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> SignUp(SignUpDto request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var identityResult= await _userManager.CreateAsync(new() { UserName = request.UserName, Email = request.Email, PhoneNumber = request.Phone, NormalizedEmail = request.Email }, request.PasswordConfirm);
+
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Kayıt Başarılı. Otomatik Yönlendirme Aktif";
+                return RedirectToAction(nameof(LoginController.SignUp));
+            }
+
+            foreach (IdentityError item in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, item.Description);
+            }
+            return View();
+        }
+    }
+}
---
+++ IdentityFrameworkWepApp/Dtos/SignUpDto.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityFrameworkWepApp.Dtos
+{
+    public class SignUpDto
+    {
+        public SignUpDto()
+        {
+
+        }
+        public SignUpDto(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+        [Required(ErrorMessage ="* Bu Alanın Doldurulması Zorunludur")]
+        [Display(Name = "*Kullanıcı Adı :")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "* Bu Alanın Doldurulması Zorunludur")]
+        [Display(Name = "*Mail :")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Bu Alanın Doldurulması Zorunludur")]
+        [Display(Name = "*Telefon :")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "* Bu Alanın Doldurulması Zorunludur")]
+        [Display(Name = "*Şifre :")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password),ErrorMessage ="Şifreler Uyuşmuyor.!")]
+        [Required(ErrorMessage = "* Bu Alanın Doldurulması Zorunludur")]
+        [Display(Name = "*Şifre Tekrar :")]
+        public string PasswordConfirm { get; set; }
+    }
+}
